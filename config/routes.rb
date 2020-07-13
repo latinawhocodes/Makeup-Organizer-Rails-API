3b7cfab24f4db798042053192e74945e0811e39a
StephanieAlyssa <alyssa.yesilyurt@gmail.com>
2020-07-13 10:34:50 -0400

    fix routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-namespace :api do 
-  resources :categories do
-    resources :products
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do 
+    resources :categories do
+      resources :products
+    end
+  end
 end
 
 =begin 
