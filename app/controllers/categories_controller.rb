4946c4b884d01317f31e9ff6d154b17403ce0809
StephanieAlyssa <alyssa.yesilyurt@gmail.com>
2020-07-13 20:46:24 -0400

    update controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,42 @@
 class Api::CategoriesController < ApplicationController
+
+    def index 
+        @categories = Category.all 
+        render json: @categories
+    end 
+
+    def show 
+        find_category
+        render json: @category
+    end
+
+    def create 
+        @category = Category.new(category_params)
+        if @category.save 
+            render json: @category 
+        else 
+            render json: {message: "ERROR"}
+        end
+    end
+
+    # def update
+        
+    # end
+
+    def destroy
+        find_category
+        @category.destroy 
+        
+    end
+
+
+    private 
+    def find_category
+        @category = Category.find(params[:id])
+    end
+
+    def category_params
+        params.require(:category).permit(:category_name)
+    end
+    
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,37 @@
 class Api::ProductsController < ApplicationController
+    #before_action: set_category
+
+    def index 
+        @products = Product.all 
+        render json: @products 
+    end 
+
+    def show 
+        find_product
+        render json: @product
+    end
+
+    def create 
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+
+    private 
+    def find_product
+        @product = Product.find(params[:id])
+    end
+
+    def set_category 
+
+    end
+
+    def product_params
+        params.require(:product).permit(:category_id, :price, :product_name, :source_name)
+    end
+
 end
